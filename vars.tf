2246e952c9b3e79a947c6d6b427e536a18f50d02
Vladimir Bogretsov <v.bogretsov@itransition.com>
2021-03-02 13:49:36 +0300

    health check updated to http

--- vars.tf
+++ vars.tf
@@ -125,6 +125,7 @@ variable "clusters" {
       interval            = number
       healthy_threshold   = number
       unhealthy_threshold = number
+      path                = string
     })
   }))
   description = "List of target groups."
